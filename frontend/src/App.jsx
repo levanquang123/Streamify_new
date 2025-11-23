d34776d87831a2f9ae6d793e4f9cd0df345051f6
levanquang123 <levanquang27122005@gmail.com>
2025-11-23 20:15:25 +0700

    button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const App = () => {
-  return <div className='bg-red-500 h-screen' >
-      App
+  return <div className='bg-red-500 h-screen'data-theme="night" >
+      <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
     </div>;
 };
 
