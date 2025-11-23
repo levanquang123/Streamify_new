2bb41441ae6a707f87c0ce77508e18df5f6d8000
levanquang123 <levanquang27122005@gmail.com>
2025-11-23 20:46:29 +0700

    page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import {Route, Routes } from "react-router";
+
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
 
 const App = () => {
-  return <div className='bg-red-500 h-screen'data-theme="night" >
-      <button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
-    </div>;
+  return <div className="h-screen" data-theme="night">
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/notifications" element={<NotificationsPage />} />
+      <Route path="/call" element={<CallPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/onboarding" element={<OnboardingPage />} />
+    </Routes>
+  </div>;
 };
 
-export default App
+export default App;
